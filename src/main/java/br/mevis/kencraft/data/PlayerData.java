f9ccb4f4d7a5afeda47c8fce5f939992d8eb7ecc
mevisnascimento495-cmd <mevisnascimento495@gmail.com>
2026-08-11 15:11:16 -0300

    Add persistent player data for Alpha 0.5.0

---
+++ src/main/java/br/mevis/kencraft/data/PlayerData.java
@@ -0,0 +1,31 @@
+package br.mevis.kencraft.data;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+public record PlayerData(Race race, int jio, int maxJio) {
+    public static final PlayerData DEFAULT = new PlayerData(Race.NONE, 0, 0);
+
+    private static final Codec<Race> RACE_CODEC =
+            Codec.STRING.xmap(Race::valueOf, Race::name);
+
+    public static final Codec<PlayerData> CODEC = RecordCodecBuilder.create(instance ->
+            instance.group(
+                    RACE_CODEC.fieldOf("race").forGetter(PlayerData::race),
+                    Codec.INT.fieldOf("jio").forGetter(PlayerData::jio),
+                    Codec.INT.fieldOf("maxJio").forGetter(PlayerData::maxJio)
+            ).apply(instance, PlayerData::new)
+    );
+
+    public boolean hasRace() {
+        return race != Race.NONE;
+    }
+
+    public static PlayerData forRinka() {
+        return new PlayerData(Race.RINKA, 0, 0);
+    }
+
+    public static PlayerData forHuman() {
+        return new PlayerData(Race.HUMAN, 100, 100);
+    }
+}
