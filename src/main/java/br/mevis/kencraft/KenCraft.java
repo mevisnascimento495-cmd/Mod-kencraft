8147d4ca11e4a768ffb0ba33617cc3baeec049a9
mevisnascimento495-cmd <mevisnascimento495@gmail.com>
2026-08-11 20:46:10 -0300

    KenCraft: registrar entidades e atributos dos novos NPCs

--- src/main/java/br/mevis/kencraft/KenCraft.java
+++ src/main/java/br/mevis/kencraft/KenCraft.java
@@ -1,11 +1,17 @@
 package br.mevis.kencraft;
 
 import br.mevis.kencraft.data.ModAttachments;
+import br.mevis.kencraft.entity.ArfInvestigatorEntity;
+import br.mevis.kencraft.entity.KenCraftEntities;
+import br.mevis.kencraft.entity.RinkaEntity;
 import br.mevis.kencraft.event.ChatSelectionHandler;
 import br.mevis.kencraft.event.PlayerLoginHandler;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 
 @Mod(KenCraft.MOD_ID)
 public class KenCraft {
@@ -13,7 +19,19 @@ public class KenCraft {
 
     public KenCraft(IEventBus modEventBus) {
         ModAttachments.ATTACHMENT_TYPES.register(modEventBus);
+        KenCraftEntities.ENTITY_TYPES.register(modEventBus);
         NeoForge.EVENT_BUS.register(PlayerLoginHandler.class);
         NeoForge.EVENT_BUS.register(ChatSelectionHandler.class);
+    }
+
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+    public static final class ModEvents {
+        private ModEvents() {}
+
+        @SubscribeEvent
+        public static void createAttributes(EntityAttributeCreationEvent event) {
+            event.put(KenCraftEntities.RINKA.get(), RinkaEntity.createAttributes().build());
+            event.put(KenCraftEntities.ARF_INVESTIGATOR.get(), ArfInvestigatorEntity.createAttributes().build());
+        }
     }
 }
