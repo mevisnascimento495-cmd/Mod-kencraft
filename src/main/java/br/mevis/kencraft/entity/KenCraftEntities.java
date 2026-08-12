a646244f6146177f0881247da896333fa424e9ce
mevisnascimento495-cmd <mevisnascimento495@gmail.com>
2026-08-12 14:29:59 -0300

    Register Rank C Rinka entity

--- src/main/java/br/mevis/kencraft/entity/KenCraftEntities.java
+++ src/main/java/br/mevis/kencraft/entity/KenCraftEntities.java
@@ -13,23 +13,23 @@ public final class KenCraftEntities {
 
     public static final DeferredHolder<EntityType<?>, EntityType<RinkaEntity>> RINKA = ENTITY_TYPES.register("rinka",
             () -> EntityType.Builder.of(RinkaEntity::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.8F)
-                    .build("kencraft:rinka"));
+                    .sized(0.6F, 1.8F).build("kencraft:rinka"));
+
+    public static final DeferredHolder<EntityType<?>, EntityType<RankCRinkaEntity>> RANK_C_RINKA = ENTITY_TYPES.register("rank_c_rinka",
+            () -> EntityType.Builder.of(RankCRinkaEntity::new, MobCategory.MONSTER)
+                    .sized(0.7F, 1.9F).build("kencraft:rank_c_rinka"));
 
     public static final DeferredHolder<EntityType<?>, EntityType<RishinEntity>> RISHIN = ENTITY_TYPES.register("rishin",
             () -> EntityType.Builder.of(RishinEntity::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.8F)
-                    .build("kencraft:rishin"));
+                    .sized(0.6F, 1.8F).build("kencraft:rishin"));
 
     public static final DeferredHolder<EntityType<?>, EntityType<ArfInvestigatorEntity>> ARF_INVESTIGATOR = ENTITY_TYPES.register("arf_investigator",
             () -> EntityType.Builder.of(ArfInvestigatorEntity::new, MobCategory.CREATURE)
-                    .sized(0.6F, 1.8F)
-                    .build("kencraft:arf_investigator"));
+                    .sized(0.6F, 1.8F).build("kencraft:arf_investigator"));
 
     public static final DeferredHolder<EntityType<?>, EntityType<ArfGeneralEntity>> ARF_GENERAL = ENTITY_TYPES.register("arf_general",
             () -> EntityType.Builder.of(ArfGeneralEntity::new, MobCategory.CREATURE)
-                    .sized(0.6F, 1.8F)
-                    .build("kencraft:arf_general"));
+                    .sized(0.6F, 1.8F).build("kencraft:arf_general"));
 
     private KenCraftEntities() {}
 }
