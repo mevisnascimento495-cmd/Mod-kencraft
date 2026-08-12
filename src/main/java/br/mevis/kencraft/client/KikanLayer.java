24e46efba60445cd31c947876fadba39081a4538
mevisnascimento495-cmd <mevisnascimento495@gmail.com>
2026-08-12 10:35:14 -0300

    Render Kikan models on players

---
+++ src/main/java/br/mevis/kencraft/client/KikanLayer.java
@@ -0,0 +1,45 @@
+package br.mevis.kencraft.client;
+
+import br.mevis.kencraft.data.ModAttachments;
+import br.mevis.kencraft.data.PlayerData;
+import br.mevis.kencraft.data.Race;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.resources.ResourceLocation;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+
+public final class KikanLayer extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
+    private static final ResourceLocation TEXTURE =
+            ResourceLocation.fromNamespaceAndPath("kencraft", "textures/entity/kikan.png");
+    private final KikanModel model;
+
+    public KikanLayer(RenderLayerParent<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> parent, EntityModelSet modelSet) {
+        super(parent);
+        this.model = new KikanModel(modelSet.bakeLayer(KenCraftEntityRenderers.KIKAN_LAYER));
+    }
+
+    @Override
+    public void render(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight,
+                       AbstractClientPlayer player, float limbSwing, float limbSwingAmount,
+                       float partialTick, float ageInTicks, float netHeadYaw, float headPitch) {
+        PlayerData data = player.getData(ModAttachments.PLAYER_DATA);
+        if (data.race() != Race.RINKA || data.kikanType() == null || data.kikanType().isBlank()) {
+            return;
+        }
+
+        model.setType(data.kikanType());
+        model.animate(KikanAnimationState.progress(), KikanAnimationState.isHeavy());
+
+        poseStack.pushPose();
+        poseStack.translate(0.0D, 0.72D, 0.24D);
+        model.renderToBuffer(poseStack, bufferSource.getBuffer(RenderType.entityCutoutNoCull(TEXTURE)),
+                packedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        poseStack.popPose();
+    }
+}
