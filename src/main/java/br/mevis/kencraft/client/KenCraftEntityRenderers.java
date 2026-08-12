869f6ae89982a44f9e32a197aa84f99389eb7c4d
mevisnascimento495-cmd <mevisnascimento495@gmail.com>
2026-08-11 23:14:51 -0300

    Register Rishin humanoid renderer

--- src/main/java/br/mevis/kencraft/client/KenCraftEntityRenderers.java
+++ src/main/java/br/mevis/kencraft/client/KenCraftEntityRenderers.java
@@ -5,6 +5,7 @@ import br.mevis.kencraft.entity.ArfGeneralEntity;
 import br.mevis.kencraft.entity.ArfInvestigatorEntity;
 import br.mevis.kencraft.entity.KenCraftEntities;
 import br.mevis.kencraft.entity.RinkaEntity;
+import br.mevis.kencraft.entity.RishinEntity;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -24,6 +25,7 @@ public final class KenCraftEntityRenderers {
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(KenCraftEntities.RINKA.get(), RinkaRenderer::new);
+        event.registerEntityRenderer(KenCraftEntities.RISHIN.get(), RishinRenderer::new);
         event.registerEntityRenderer(KenCraftEntities.ARF_INVESTIGATOR.get(), ArfRenderer::new);
         event.registerEntityRenderer(KenCraftEntities.ARF_GENERAL.get(), GeneralRenderer::new);
     }
@@ -35,6 +37,17 @@ public final class KenCraftEntityRenderers {
 
         @Override
         public ResourceLocation getTextureLocation(RinkaEntity entity) {
+            return STEVE_TEXTURE;
+        }
+    }
+
+    private static final class RishinRenderer extends HumanoidMobRenderer<RishinEntity, HumanoidModel<RishinEntity>> {
+        private RishinRenderer(EntityRendererProvider.Context context) {
+            super(context, new HumanoidModel<>(context.bakeLayer(ModelLayers.PLAYER)), 0.5F);
+        }
+
+        @Override
+        public ResourceLocation getTextureLocation(RishinEntity entity) {
             return STEVE_TEXTURE;
         }
     }
