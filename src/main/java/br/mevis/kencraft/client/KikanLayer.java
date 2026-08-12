ae4417cd24cc0df8fed1e655956e967880bcf40d
mevisnascimento495-cmd <mevisnascimento495@gmail.com>
2026-08-12 10:39:52 -0300

    Fix Kikan model rendering API

--- src/main/java/br/mevis/kencraft/client/KikanLayer.java
+++ src/main/java/br/mevis/kencraft/client/KikanLayer.java
@@ -3,6 +3,7 @@ package br.mevis.kencraft.client;
 import br.mevis.kencraft.data.ModAttachments;
 import br.mevis.kencraft.data.PlayerData;
 import br.mevis.kencraft.data.Race;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.player.AbstractClientPlayer;
@@ -10,9 +11,8 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.resources.ResourceLocation;
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
 
 public final class KikanLayer extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
     private static final ResourceLocation TEXTURE =
@@ -39,7 +39,7 @@ public final class KikanLayer extends RenderLayer<AbstractClientPlayer, PlayerMo
         poseStack.pushPose();
         poseStack.translate(0.0D, 0.72D, 0.24D);
         model.renderToBuffer(poseStack, bufferSource.getBuffer(RenderType.entityCutoutNoCull(TEXTURE)),
-                packedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+                packedLight, OverlayTexture.NO_OVERLAY, 0xFFFFFFFF);
         poseStack.popPose();
     }
 }
