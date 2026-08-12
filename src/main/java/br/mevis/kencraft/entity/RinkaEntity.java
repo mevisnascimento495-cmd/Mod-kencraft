de8bae8c5f78da16a5ab801a35779dfc3f323b7f
mevisnascimento495-cmd <mevisnascimento495@gmail.com>
2026-08-12 10:37:19 -0300

    Use dedicated Rinka skin without armor overlay

--- src/main/java/br/mevis/kencraft/entity/RinkaEntity.java
+++ src/main/java/br/mevis/kencraft/entity/RinkaEntity.java
@@ -1,6 +1,5 @@
 package br.mevis.kencraft.entity;
 
-import java.util.List;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -12,26 +11,12 @@ import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.item.DyeItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.component.DyedItemColor;
 import net.minecraft.world.level.Level;
 
 public class RinkaEntity extends Monster {
     public RinkaEntity(EntityType<? extends Monster> type, Level level) {
         super(type, level);
         this.setPersistenceRequired();
-        this.setItemSlot(EquipmentSlot.CHEST, dyed(Items.LEATHER_CHESTPLATE, DyeColor.RED));
-        this.setItemSlot(EquipmentSlot.LEGS, dyed(Items.LEATHER_LEGGINGS, DyeColor.RED));
-        this.setItemSlot(EquipmentSlot.FEET, dyed(Items.LEATHER_BOOTS, DyeColor.BLACK));
-    }
-
-    private static ItemStack dyed(net.minecraft.world.item.Item item, DyeColor color) {
-        ItemStack stack = new ItemStack(item);
-        return DyedItemColor.applyDyes(stack, List.of(DyeItem.byColor(color)));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
