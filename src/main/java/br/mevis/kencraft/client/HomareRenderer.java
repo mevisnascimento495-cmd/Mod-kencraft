01067087c5cb40a125bc5077cead7d4563c9f941
mevisnascimento495-cmd <mevisnascimento495@gmail.com>
2026-08-16 15:04:42 -0300

    Refaz completamente as texturas do Shin e da Kaori

--- src/main/java/br/mevis/kencraft/client/HomareRenderer.java
+++ src/main/java/br/mevis/kencraft/client/HomareRenderer.java
@@ -2,17 +2,17 @@ package br.mevis.kencraft.client;
 
 import br.mevis.kencraft.KenCraft;
 import br.mevis.kencraft.entity.KenCraftEntities;
-import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-public final class HomareRenderer extends HumanoidMobRenderer<KenCraftEntities.HomareEntity, HumanoidModel<KenCraftEntities.HomareEntity>> {
+public final class HomareRenderer extends HumanoidMobRenderer<KenCraftEntities.HomareEntity, PlayerModel<KenCraftEntities.HomareEntity>> {
     private static final ResourceLocation SHIN_TEXTURE = ResourceLocation.fromNamespaceAndPath(KenCraft.MOD_ID, "textures/entity/shin_homare.png");
     private static final ResourceLocation KAORI_TEXTURE = ResourceLocation.fromNamespaceAndPath(KenCraft.MOD_ID, "textures/entity/kaori_homare.png");
 
     public HomareRenderer(EntityRendererProvider.Context context) {
-        super(context, new HumanoidModel<>(context.bakeLayer(net.minecraft.client.model.geom.ModelLayers.PLAYER)), 0.5F);
+        super(context, new PlayerModel<>(context.bakeLayer(net.minecraft.client.model.geom.ModelLayers.PLAYER), false), 0.5F);
     }
 
     @Override
